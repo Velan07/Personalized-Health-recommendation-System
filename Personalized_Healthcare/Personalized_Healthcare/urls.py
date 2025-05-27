9f31ba64247d9a9da7d8a9cae4a0fd0ae2856b6f
velan-07 <velan00777@gmail.com>
2025-05-27 09:36:29 +0530

    Symptom Based Disease Prediction Done

--- Personalized_Healthcare/Personalized_Healthcare/urls.py
+++ Personalized_Healthcare/Personalized_Healthcare/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('prediction.urls')),	
+    path('disease/', include('disease_prediction.urls')),
 ]
